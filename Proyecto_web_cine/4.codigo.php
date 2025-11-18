8b1888c4909b4974c97c89146e7b97534643125b
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-11-18 11:55:39 +0100

    inicio qr

--- Proyecto_web_cine/4.codigo.php
+++ Proyecto_web_cine/4.codigo.php
@@ -1,2 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+require 'libs/phpqrcode/qrlib.php';
+
+$text = "http://localhost/proyecto_web_cine/entrada.php?nombre=Antonio&asiento=1&cine=los_arcos";
+QRcode::png($text);
